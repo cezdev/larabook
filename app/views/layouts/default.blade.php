eec999414c62ecb656da9f447212739a668f1f05
Andrej Cezner <acezner@gmail.com>
2014-07-04 13:29:25 +0200

    First commit
    Clone of Facebook

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Larabook</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+<scrip src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></scrip>
+</body>
+</html>
